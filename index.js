ab4de457823a9524aae69717af47ef6fabd19a5d
Igor-Lira <56862592+Igor-Lira@users.noreply.github.com>
2021-06-30 19:39:34 -0300

    Servidor Web rodando com Express

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require ('express')
+
+const app = express ()
+
+app.get ('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos"))
+app.listen  (3000, () => console.log ("Servidor na porta 3000") )
