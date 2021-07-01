04cb623c2da6f3512f0fa1b4ad62bc858245c65f
Igor-Lira <56862592+Igor-Lira@users.noreply.github.com>
2021-06-30 21:30:27 -0300

    Conectando com base de dados mysql

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 const customExpress = require ('./config/customExpress')
+const conexao = require ('./infraestrutura/conexao')
+const Tabelas = require ('./infraestrutura/tabelas')
 
-const app = customExpress()
-app.get ('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos, !!"))
-app.listen  (3000, () => console.log ("Servidor na porta 3000") )
+conexao.connect(err => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log ('conectado com sucesso')
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.get ('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos, !!"))
+        app.listen  (3000, () => console.log ("Servidor na porta 3000") )
+    }
+})
